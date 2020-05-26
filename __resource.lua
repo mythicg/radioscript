85848e6fdb1c8c7e36ecb0db7ef93ecb2cff2bfd
Mythic <61439974+MythicGaming5900@users.noreply.github.com>
2020-05-25 21:08:26 -0600

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,6 @@
+--Script Made By Mythic
+--discord: Michael M.#1159
+
+client_script "client.lua"
+
+server_script "sv.lua"
---
+++ client.lua
@@ -0,0 +1,14 @@
+--Script Made By Mythic aka Michael 
+
+RegisterCommand("radio", function(source, args, rawCommand)
+    local message = table.concat(args, " ", 1)
+
+    TriggerEvent("chatMessage", "[Radio]", GetPlayerName(source), {10, 117, 20}, text )
+end)
+--Script Made By Mythic aka Michael 
+
+RegisterCommand("radio", function(source, args, rawCommand)
+    local message = table.concat(args, " ", 1)
+
+    TriggerEvent("chatMessage", " [ ^2 Radio ]" .. "  " .. GetPlayerName(source) .."   " .. "^7: " .. "      ".. message)
+end)
