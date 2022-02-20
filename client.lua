5460bf4cc73ba267dfc94e23b75464181debc304
Mythic <61439974+MythicGaming5900@users.noreply.github.com>
2022-02-19 22:07:32 -0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,11 +1,11 @@
---Script Made By Mythic aka Michael 
+--Script Made By Mythic 
 
 RegisterCommand("radio", function(source, args, rawCommand)
     local message = table.concat(args, " ", 1)
 
     TriggerEvent("chatMessage", "[Radio]", GetPlayerName(source), {10, 117, 20}, text )
 end)
---Script Made By Mythic aka Michael 
+--Script Made By Mythic
 
 RegisterCommand("radio", function(source, args, rawCommand)
     local message = table.concat(args, " ", 1)
